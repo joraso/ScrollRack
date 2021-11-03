f48d735f93cb5fafcc2f02fa2dcaa1e4027edd44
Joseph Raso <jraso@berkeley.edu>
2021-11-03 14:01:47 -0700

    Misc. code refactorization and beautification.

--- MainGUI.py
+++ MainGUI.py
@@ -24,25 +24,25 @@ class CollectionModel(QtCore.QAbstractTableModel):
         
     # Mandatory reimplementations for a table model ===========================
     def data(self, index, role):
-        # By defaul display the contents of the collection as a string
-        if role==QtCore.Qt.DisplayRole and index.column()!=self.manacolumn \
-            and index.column()!=self.selectioncolumn:
-            return str(self.collection.iloc[index.row(), index.column()])
-        # For the 'Cost' column, include no display role, and instead put the
-        # cost image as decoation role.
-        if role==QtCore.Qt.DecorationRole and index.column()==self.manacolumn:
-            return self.parseManaCost(index)
-        # The selelection column must have a checked state
-        if role==QtCore.Qt.CheckStateRole and \
-            index.column()==self.selectioncolumn:
-            if self.collection.iloc[index.row(), index.column()]:
-                return QtCore.Qt.Checked
-            else: return QtCore.Qt.Unchecked
+        # The 'Cost' column has an image as decoration role, no display role
+        if index.column()==self.manacolumn:
+            if role==QtCore.Qt.DecorationRole:
+                return self.parseManaCost(index)
+        # The selection column has a checked state, no display role
+        elif index.column()==self.selectioncolumn:
+            if role==QtCore.Qt.CheckStateRole:
+                if self.collection.iloc[index.row(), index.column()]:
+                    return QtCore.Qt.Checked
+                else: return QtCore.Qt.Unchecked
+        # For all other columns, default to collection content as display role
+        elif role==QtCore.Qt.DisplayRole:
+            return str(self.collection.iloc[index.row(), index.column()])        
     def rowCount(self, index):
         return len(self.collection)
     def columnCount(self, index):
         return len(self.collection.columns)
     def headerData(self, section, orientation, role=QtCore.Qt.DisplayRole):
+        # Display the column names in the horizontal header
         if orientation == QtCore.Qt.Horizontal and \
             role == QtCore.Qt.DisplayRole:
             return self.collection.columns[section]
@@ -57,7 +57,7 @@ class CollectionModel(QtCore.QAbstractTableModel):
         # Otherwise go to the parent definition
         else: return super().flags(index)
     def setData(self, index, value, role):
-        # if the index is the selection column, toggle it's value
+        # Define how values are translated to bool for the selection column
         if index.column() == self.selectioncolumn:
             if value==0:
                 self.collection.iloc[index.row(),index.column()] = False
@@ -105,19 +105,18 @@ class CollectionView(QtWidgets.QTableView):
         for c in range(len(columns)):
             if columns[c] in self.columnWidths.keys():
                 self.setColumnWidth(c, self.columnWidths[columns[c]])
-        # Connect the on click functionality
+        # Connect the on-click functionality
         self.clicked.connect(self.onClick)
     def onClick(self):
         """Defines what happens when the table is clicked."""
         index = self.selectionModel().currentIndex()
-#        print(f"{index.row()}, {index.column()}")
-        if index.column() == 0:
+        # if the index is the selection column, toggle it's value
+        if index.column() == self.model().selectioncolumn:
             if self.model().collection.iloc[index.row(), index.column()]:
                 self.model().setData(index, 0, QtCore.Qt.CheckStateRole)
             else: self.model().setData(index, 1, QtCore.Qt.CheckStateRole)
             
-
-        
+            
 class MainWindow(QtWidgets.QMainWindow):
     """The main GUI window. Collections are opened in tabs, with a toolbar
     along the top of the window for navigation."""
@@ -131,16 +130,18 @@ class MainWindow(QtWidgets.QMainWindow):
         self.tabs.tabCloseRequested.connect(self.closeTab)
         self.setCentralWidget(self.tabs)
         # Add the top tool bar.
-        self.addToolBar(self.toolBar())
-        self.addDockWidget(QtCore.Qt.TopDockWidgetArea, self.searchBar())
-    def toolBar(self):
+        self.generateToolBar(); self.generateSearchBar()
+    
+    # Toolbar and Searchbar generators ========================================
+    def generateToolBar(self):
         """Initializes the toolbar at the top of the window."""
         self.toolbar = QtWidgets.QToolBar()
-        self.openButton = self.toolbar.addAction("Open", self.openTab)
-        self.newButton = self.toolbar.addAction("New", self.newTab)
-        self.searchButton = self.toolbar.addAction("Scryfall",
-            lambda: self.searchbar.setHidden(False))
-        self.dropButton = self.toolbar.addAction("Drop", self.dropSelected)
+        # Add simple bottun items Open/Save/New/Scryfall
+        self.toolbar.addAction("Open", self.openTab)
+        self.toolbar.addAction("New", self.newTab)
+        self.toolbar.addAction("Scryfall", lambda:
+            self.searchbar.setHidden(False))
+        self.toolbar.addAction("Drop", self.dropSelected)
         # Adding the dropdown menu for 'Copy To' and 'Move To'
         self.copytoMenu = QtWidgets.QMenu("Copy To")
         self.copytoMenu.aboutToShow.connect(self.generateCopyToMenu)
@@ -148,16 +149,20 @@ class MainWindow(QtWidgets.QMainWindow):
         self.movetoMenu = QtWidgets.QMenu("Move To")
         self.movetoMenu.aboutToShow.connect(self.generateMoveToMenu)
         self.toolbar.addAction(self.movetoMenu.menuAction())
-        return self.toolbar
-    def searchBar(self):
+        # Add the toolbar to the main window
+        self.addToolBar(self.toolbar)
+    def generateSearchBar(self):
         """Initializes a search bar at the bottom of the window."""
         layout = QtWidgets.QHBoxLayout()
+        # Generate the line-edit for entering search queries
         self.searchfield = QtWidgets.QLineEdit()
         self.searchfield.returnPressed.connect(self.openSearch)
         layout.addWidget(self.searchfield)
+        # Generate the search pushbutton
         searchbutton = QtWidgets.QPushButton("Search")
         searchbutton.clicked.connect(self.openSearch)
         layout.addWidget(searchbutton)
+        # Wrap in a DockWidget
         searchwidget = QtWidgets.QWidget()
         searchwidget.setLayout(layout)
         self.searchbar = QtWidgets.QDockWidget()
@@ -165,9 +170,12 @@ class MainWindow(QtWidgets.QMainWindow):
         self.searchbar.setWindowTitle("Enter Scryfall query:")
         # Default to hidden upon initialization
         self.searchbar.setHidden(True)
-        return self.searchbar
+        # Add the seachbar to the main window
+        self.addDockWidget(QtCore.Qt.TopDockWidgetArea, self.searchbar)
+    
+    # Tab manipulation functions ==============================================
     def openSearch(self):
-        """Opens a new collection tab containing search results"""
+        """Opens a new collection tab containing search results."""
         view = CollectionView(Collection.from_search(self.searchfield.text()))
         self.tabs.addTab(view, view.model().collection.name)
         self.searchbar.setHidden(True) # re-hide the search bar
@@ -190,13 +198,15 @@ class MainWindow(QtWidgets.QMainWindow):
     def closeTab(self, currentIndex):
         """Closes a tab."""
         self.tabs.removeTab(currentIndex)
+        
+    # Collection editing functionalities ======================================
     def dropSelected(self):
         """Drops the selected cards from the current Collection."""
         mod = self.tabs.widget(self.tabs.currentIndex()).model()
         mod.collection.drop_selected(); mod.layoutChanged.emit()
     def generateCopyToMenu(self):
-        """Generates the dropdown menu of currently open tabs to copy selected
-        cards to."""
+        """Generates/regenerates the dropdown menu of currently open tabs to
+        copy selected cards to."""
         self.copytoMenu.clear() # Clear the current menu
         sourceIndex = self.tabs.currentIndex()
         def generate_copyto(destIndex):
@@ -210,16 +220,17 @@ class MainWindow(QtWidgets.QMainWindow):
                 # Update the view of both models
                 dest.layoutChanged.emit(); source.layoutChanged.emit()
             return copyto
-        num = self.tabs.count() # then number of tabs open
-        for i in range(num):
+        # Iterate through open tabs and add them to the list
+        for i in range(self.tabs.count()):
             if i != sourceIndex:
                 tabname = self.tabs.widget(i).model().collection.name
                 self.copytoMenu.addAction(tabname, generate_copyto(i))
+        # Copying to the current tab is treated separately
         self.copytoMenu.addSeparator()
         self.copytoMenu.addAction("Here", generate_copyto(sourceIndex))
     def generateMoveToMenu(self):
-        """Generates the dropdown menu of currently open tabs to move selected
-        cards to."""
+        """Generates/regenerates the dropdown menu of currently open tabs to
+        move selected cards to."""
         self.movetoMenu.clear() # Clear the current menu
         sourceIndex = self.tabs.currentIndex()
         def generate_moveto(destIndex):
@@ -230,14 +241,13 @@ class MainWindow(QtWidgets.QMainWindow):
                 # Perform the move to operation
                 cards = source.collection.copy_selected()
                 dest.collection.add_cards(cards)
-                # The difference between copy and move
+                # The key difference between copy and move:
                 source.collection.drop_selected() 
                 # Update the view of both models
                 dest.layoutChanged.emit(); source.layoutChanged.emit()
             return moveto
-        num = self.tabs.count() # then number of tabs open
         # Note: 'move to' provides no option to move to the source list
-        for i in range(num):
+        for i in range(self.tabs.count()):
             if i != sourceIndex:
                 tabname = self.tabs.widget(i).model().collection.name
                 self.movetoMenu.addAction(tabname, generate_moveto(i))
